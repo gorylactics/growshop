fc7daa96c772ffd3a860431113d7efcf00c5663d
adrian <adrian.miranda.r@gmail.com>
2019-04-13 21:29:03 -0300

    chance from bootstrap 3.3.7 to 4.2

--- wordpress/wp-content/themes/foca/functions.php
+++ wordpress/wp-content/themes/foca/functions.php
@@ -3,7 +3,7 @@
     function fg_enqueue_style() { 
     $theme_data = wp_get_theme();
 
-wp_register_style('bootstrap_css', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css', null, $theme_data->get('3.3.7'));
+wp_register_style('bootstrap_css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css', null, $theme_data->get('4.2'));
 wp_register_style('main', get_parent_theme_file_uri('/css/main.css'), null, null);
 
 wp_enqueue_style( 'bootstrap_css' );
@@ -13,7 +13,7 @@ add_action( 'wp_enqueue_scripts', 'fg_enqueue_style' );
 // js
     function fg_enqueue_scripts() {
     
-    wp_register_script('bootstrap_js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js', array('jquery'),'3.3.7', true);
+    wp_register_script('bootstrap_js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js', array('jquery'),'4.2', true);
     wp_register_script('scripts', get_parent_theme_file_uri('/js/script.js'), null,'4.0', true);
     
     wp_enqueue_script('jquery');
@@ -22,4 +22,3 @@ add_action( 'wp_enqueue_scripts', 'fg_enqueue_style' );
 } 
     add_action('wp_enqueue_scripts', 'fg_enqueue_scripts');
 ?>
-
