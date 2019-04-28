0792846a9022e3c2f28c9ec92168550751a3d11f
adrian <adrian.miranda.r@gmail.com>
2019-04-28 01:08:27 -0300

    test boostrap

--- wordpress/wp-content/themes/master2/home.php
+++ wordpress/wp-content/themes/master2/home.php
@@ -1,5 +1,8 @@
+<?php get_header() ?>
 <div>
 <p  class="container__fluid" style="background-color:red">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt perspiciatis itaque dolor quis aliquam assumenda, in consequatur, blanditiis voluptatum quae maiores enim sit quo eius veniam soluta laborum id ducimus.</p>
 </div>
+<div class="jumbotron">hola</div>
 
+<div class="container__fluid" style="background-color:green"><p class="card">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae reprehenderit nihil enim et facilis quisquam. Blanditiis reprehenderit vel recusandae ex voluptatibus totam laudantium atque harum eaque ipsum velit, nisi iste?</p></div>
 <?php get_footer() ?>
