1fce62b68b2aaabbf63f0d2fb76bc9138c9a5dda
adrian <adrian.miranda.r@gmail.com>
2019-04-28 01:46:21 -0300

    new page-holanda.php and menu of wp

--- wordpress/wp-content/themes/master2/home.php
+++ wordpress/wp-content/themes/master2/home.php
@@ -1,6 +1,12 @@
 <?php get_header() ?>
-<div>
-<p  class="container__fluid" style="background-color:red">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt perspiciatis itaque dolor quis aliquam assumenda, in consequatur, blanditiis voluptatum quae maiores enim sit quo eius veniam soluta laborum id ducimus.</p>
+<div class="container  card">
+<div class="row">
+<p  class="container__fluid col-6" style="background-color:red">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt perspiciatis itaque dolor quis aliquam assumenda, in consequatur, blanditiis voluptatum quae maiores enim sit quo eius veniam soluta laborum id ducimus. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia enim magni dolorem laudantium voluptates tenetur fugiat pariatur nam, ad, reprehenderit hic eveniet consectetur numquam fugit ipsa minus animi. Necessitatibus, deserunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur totam esse fugit sint temporibus facilis debitis, obcaecati, iusto, illo voluptate commodi necessitatibus minus ad? Exercitationem magni animi asperiores! Ab, animi.</p>
+
+<p  class="container__fluid col-3" style="background-color:red">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt perspiciatis itaque dolor quis aliquam assumenda, in consequatur, blanditiis voluptatum quae maiores enim sit quo eius veniam soluta laborum id ducimus.</p><p  class="container__fluid col-3" style="background-color:red">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt perspiciatis itaque dolor quis aliquam assumenda, in consequatur, blanditiis voluptatum quae maiores enim sit quo eius veniam soluta laborum id ducimus.</p><p  class="container__fluid col-3" style="background-color:red">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt perspiciatis itaque dolor quis aliquam assumenda, in consequatur, blanditiis voluptatum quae maiores enim sit quo eius veniam soluta laborum id ducimus.</p><p  class="container__fluid col-3" style="background-color:red">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt perspiciatis itaque dolor quis aliquam assumenda, in consequatur, blanditiis voluptatum quae maiores enim sit quo eius veniam soluta laborum id ducimus.</p>
+
+
+</div>
 </div>
 <div class="jumbotron">hola</div>
 
