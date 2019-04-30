7fe5aa1a5344917b8ccfd97dbccc8760c36cdc92
adrian <adrian.miranda.r@gmail.com>
2019-04-30 01:07:14 -0300

    boostrap broke ;)

--- wordpress/wp-content/themes/master3/header.php
+++ wordpress/wp-content/themes/master3/header.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
 	<a class="navbar-brand" href="#">
 		<img src="<?php bloginfo('template_url')?>/assets/images/logos/foca_sin_fondo.png" alt="logo" class="logo"></a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
