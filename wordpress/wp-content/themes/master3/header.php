fdd5456bb5eec8d3678a1f57173a77bc48b91cee
adrian <adrian.miranda.r@gmail.com>
2019-04-30 01:00:02 -0300

    chance color of boostrap

--- wordpress/wp-content/themes/master3/header.php
+++ wordpress/wp-content/themes/master3/header.php
@@ -11,15 +11,15 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<a class="navbar-brand" href="#">
-		<img src="<?php bloginfo('template_url')?>/assets/images/logos/foca_sin_fondo.png" alt="logo" class="logo"></a></a>
+		<img src="<?php bloginfo('template_url')?>/assets/images/logos/foca_sin_fondo.png" alt="logo" class="logo"></a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   	</button>
 
 	<div class="collapse navbar-collapse" id="navbarSupportedContent">
     	<ul class="navbar-nav mr-auto">
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SEMILLAS</a>
+			<li class="nav-item dropdown ">
+				<a class="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">SEMILLAS</a>
 				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
 					<a class="dropdown-item" href="#">FEMINIZADAS</a>
 					<div class="dropdown-divider"></div>
