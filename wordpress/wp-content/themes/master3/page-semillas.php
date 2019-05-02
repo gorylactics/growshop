c855657e1c11384962fd1a7fab9c1dea762bf0d2
adrian <adrian.miranda.r@gmail.com>
2019-05-02 19:14:02 -0300

    fix problem whit wp dont can create a new page

---
+++ wordpress/wp-content/themes/master3/page-semillas.php
@@ -0,0 +1,5 @@
+<?php get_header() ?>
+
+<h1><a href="localhost:8888/">  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias architecto, saepe earum nemo quidem iusto perspiciatis ratione. Provident excepturi quo labore eveniet commodi! Voluptatem consequatur libero iure laboriosam iste error.</a></h1>
+<img src="<?php bloginfo('template_url')?>/assets/images/logos/foca_sin_fondo.png" alt="logo" class="logo"></a>
+
