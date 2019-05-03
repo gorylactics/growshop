48e379728cbdc9557e9b9c42059be758c392727c
adrian <adrian.miranda.r@gmail.com>
2019-05-02 23:21:46 -0300

    forwad

--- wordpress/wp-content/themes/master3/page-parafernalia.php
+++ wordpress/wp-content/themes/master3/page-parafernalia.php
@@ -1,3 +1,15 @@
 <?php get_header() ?>
 
 <h1>hola</h1>
+
+<div class="container"> 
+    <div class="row">
+        <div class="card col-lg-3 ">
+        <img src="<?php bloginfo('template_url')?>/assets/images/logos/foca_sin_fondo.png" alt="logo" class="logo"></a>
+</div>
+    </div>
+</div>
+
+<?php get_footer() ?>
+
+<!-- dirigir a simple.php -->
