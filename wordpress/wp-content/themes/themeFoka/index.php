baa96c779783f0810d3e42b80a86e3ebc738d529
adrian <adrian.miranda.r@gmail.com>
2019-05-07 22:37:49 -0300

    fix bugs in single.php

--- wordpress/wp-content/themes/themeFoka/index.php
+++ wordpress/wp-content/themes/themeFoka/index.php
@@ -1,40 +1,38 @@
-<?php get_header() ?>
+<?php get_header(); ?>
 
 <div class="row">
     <div class="col-lg-9">
 <!-- entradas -->
-
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <div class="card-body">
-        <a href="<?php the_permalink() ?>">
-            <h2><?php the_title()?></h2>
-        </a>
-            <p class="small mb-0">fecha: <?php the_time('F j, Y'); ?></p>
-            <p class="small mb-0">autor: <?php the_author(); ?></p>
-            <p class="small mb-0">etiquetas: <?php the_tags('', '/', ''); ?>
-            <p class="small mb-0">categoria: <?php the_category('/'); ?> 
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <div class="card-body">
+                <a href="<?php the_permalink() ?>">
+                    <h2>
+                        <?php the_title()?>
+                    </h2>
+                </a>
+                <p class="small mb-0">fecha: <?php the_time('F j, Y'); ?></p>
+                <p class="small mb-0">autor: <?php the_author(); ?></p>
+                <p class="small mb-0">etiquetas: <?php the_tags('', '/', ''); ?>
+                <p class="small mb-0">categoria: <?php the_category('/'); ?> 
         
-            <?php if ( has_post_thumbnail() ) {
-                the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid mb-3'));}?>
-
-                <?php the_excerpt() ?>
-                <a href="<?php the_permalink() ?>" class="btn btn-primary">ir al articulo</a>
-    </div>
-              
-        
+        <?php if ( has_post_thumbnail() ) {
+            the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid mb-3'));}?>
+            <?php the_excerpt()?>
+            <a href="<?php the_permalink()?>" class="btn btn-primary btn-block">ir al articulo
+            </a>
+        </div>
 <?php endwhile; endif; ?>
 <!-- /entradas -->
 
 <!-- paginacion -->
-    
-        <div class="card-body">
-            <?php get_template_part('template-parts/content', 'paginacion'); ?>
-        </div>
-    
+    <div class="card-body">
+        <?php get_template_part('template-parts/content', 'paginacion'); ?>
+    </div>  
 <!-- /paginacion -->
 
-             
 </div>
+<!-- linea 33 div de cierre index.php -->
 
-<?php get_sidebar() ?>
-<?php get_footer() ?>
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
