416a2c4faf0503e9acc77328e148f2347bc6dfb3
adrian <adrian.miranda.r@gmail.com>
2019-05-13 00:39:22 -0400

    fix bugs

--- wordpress/wp-content/themes/themeFoka/single.php
+++ wordpress/wp-content/themes/themeFoka/single.php
@@ -5,7 +5,7 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     
         <div class="row">
-            <div class="col-lg-9">
+            
                 <div class="card-body">
                     <h2>
                         <?php the_title() ?>
@@ -22,7 +22,7 @@
                     <p class="small mb-0">etiquetas: <?php the_tags('', '/', ''); ?><br>
                 </div>
             </div>
-        </div>
+       
     
 <?php endwhile; endif; ?>
 </div>
