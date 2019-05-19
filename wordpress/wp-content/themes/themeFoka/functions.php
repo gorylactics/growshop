a5ab00ae8dd87e665cac65eb2f9ba0f7e876e6b1
adrian <adrian.miranda.r@gmail.com>
2019-05-19 17:12:53 -0400

    fix route css

--- wordpress/wp-content/themes/themeFoka/functions.php
+++ wordpress/wp-content/themes/themeFoka/functions.php
@@ -7,7 +7,7 @@
         
         wp_register_style('style' ,get_stylesheet_uri());
         wp_register_style('bootstrap_css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', null, null);
-        wp_register_style('main', get_parent_theme_file_uri('/css/main.css'), null, null);
+        wp_register_style('main', get_parent_theme_file_uri('assets/css/main.css'), null, null);
 
         wp_enqueue_style( 'style' );
         wp_enqueue_style( 'bootstrap_css' );
