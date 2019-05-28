ad64b11cca8da31119d3ca4033ef9f4360432109
adrian <adrian.miranda.r@gmail.com>
2019-05-27 20:36:52 -0400

    fix front-page post for category correct

--- wordpress/wp-content/themes/themeFoka/front-page.php
+++ wordpress/wp-content/themes/themeFoka/front-page.php
@@ -3,52 +3,33 @@
 
 <div class="row">
     <div class="col-lg-9">
-        <h1>holaaaa
-        </h1>
+        <h1>holaaaa</h1>
+            <?php 
+            // The Query
+            $the_query = new WP_Query( array( 'posts_per_page' => 10,  'category_name' => 'automaticas,bongs')); 
 
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <?php endwhile; endif; ?>
-
-
-<?php
-
-// The Query
-$the_query = new WP_Query( array( 'posts_per_page' => 10, 'offset' => 1, 'category_name' => 'automaticas')); 
-
-
-// The Loop
-if ( $the_query->have_posts() ) : ?>
-	<ul>
-	<?php while ( $the_query->have_posts() ) :
-		$the_query->the_post(); ?>
-		
-       
-        <div class="card-body entradas col-lg-4">
-            <?php the_category() ?>
-                <a href="<?php the_permalink() ?>">
-                <?php if ( has_post_thumbnail() ) {
-            the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid mb-3'));}?>  
-                        
-                    
-                </a>
+            // The Loop
+            if ( $the_query->have_posts() ) : ?>
                 
-        
-        
-        
-            
-            <hr>
-        </div>
-	    <?php endwhile ?>
-	</ul>
-
-    
-	
-    <?php wp_reset_postdata();
-        else : ?>
-	        <p>no posts found </p> 
-    <?php endif; ?>
-
-    <div></div>
+                <?php while ( $the_query->have_posts() ) :
+                    $the_query->the_post(); ?>
+                    <div class="card-body entradas col-lg-4">
+                        <?php the_category() ?>
+                        <a href="<?php the_permalink() ?>">
+                            <?php if ( has_post_thumbnail() ) 
+                                {the_post_thumbnail('post-thumbnails' , array('class' => 'img-fluid mb-3'));
+                                }
+                            ?>  
+                        </a>
+                        <hr>
+                    </div>
+                <?php endwhile ?>
+                <?php wp_reset_postdata();
+                    else : ?>
+                        <p>no posts found </p> 
+                <?php endif; ?>
+        <div>
+    </div>
 </div>
 
 
