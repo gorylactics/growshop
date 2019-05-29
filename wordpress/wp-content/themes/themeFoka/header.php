646b426066cd8a2e6483f073841ca003a9f2dd7a
adrian <adrian.miranda.r@gmail.com>
2019-05-28 23:08:28 -0400

    clean code in progress

--- wordpress/wp-content/themes/themeFoka/header.php
+++ wordpress/wp-content/themes/themeFoka/header.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>foca growshop</title>
@@ -9,15 +8,14 @@
   </head>
 
   <body <?php body_class(); ?>>
-   
-    <!-- navbar -->
+  <!-- navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark" >
         <section class="container">
             <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
               <img src="<?php bloginfo('template_url')?>/assets/images/foca_sin_fondo.png" alt="logo" class="logo" >
             </a>
 
-            <h2  style="margin-left: 15%;">foca growshop </h2>
+            <h1 style="margin-left: 15%;">foca growshop </h1>
             
             <button 
                 class="navbar-toggler " 
@@ -32,25 +30,19 @@
             
             </button>
          
-    <?php
-      wp_nav_menu( array(
-        'theme_location'    => 'menu-principal',
-        'depth'             => 5,
-        'container'         => 'div',
-        'container_class'   => 'collapse navbar-collapse',
-        'container_id'      => 'navbarSupportedContent',
-        'menu_class'        => 'nav navbar-nav ml-auto',
-        'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-        'walker'            => new WP_Bootstrap_Navwalker(),
-      ) );
-      ?>
-        </section>
-    </nav>
-    <!--  /navbar -->
+          <?php
+            wp_nav_menu( array(
+              'theme_location'    => 'menu-principal',
+              'depth'             => 5,
+              'container'         => 'div',
+              'container_class'   => 'collapse navbar-collapse',
+              'container_id'      => 'navbarSupportedContent',
+              'menu_class'        => 'nav navbar-nav ml-auto',
+              'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+              'walker'            => new WP_Bootstrap_Navwalker(),
+            ) );
+            ?>
+      </section>
+  </nav>
+<!--  /navbar -->
 
-
-
-    <section class="container">
-        <h2 class="my-5 text-center"><?php echo category_description(); ?></h2>
-    </section>
-  
