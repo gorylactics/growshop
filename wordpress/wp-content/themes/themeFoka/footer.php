7e241f5e74dd9761b8f0d3ed877764bd6c178a89
adrian <adrian.miranda.r@gmail.com>
2019-07-08 23:05:47 -0400

    ending

--- wordpress/wp-content/themes/themeFoka/footer.php
+++ wordpress/wp-content/themes/themeFoka/footer.php
@@ -1,10 +1,35 @@
  <!-- footer -->
- <footer class="container_fluid py-4 text-center text-light bg-dark footer">
- <div class="row">   
- <div class="col-lg-4"><p> Contactanos </p></div>
-    <div class="col-lg-4"><p>www.focagrow.cl</p></div>
-    <div class="col-lg-4"><p>Siguenos</p></div>
+ <footer class="container__fluid py-4 text-center text-light bg-none footer col-lg-12">
+  <div class="row">
+    <div class="container col-lg-12 padre">
+    <div class="col-lg-4 rrss">
+      <a class="navbar-brand" href="<?php echo esc_url(('+56994708649')); ?>">
+        <img src="<?php bloginfo('template_url')?>/assets/images/whatsapp-brands.svg" alt="rrss" class="rrss" >
+      </a>
+    <hr>
+    <br>
+    <p> Contactanos </p>
+  </div>
+
+    <div class="col-lg-4 rrss">
+    <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
+        <img src="<?php bloginfo('template_url')?>/assets/images/globe-americas-solid.svg" alt="rrss" class="rrss" >
+      </a>
+    <hr>
+    <br>
+      <p> www.focagrow.cl </p>
     </div>
+
+    <div class="col-lg-4 rrss">
+    <a class="navbar-brand" href="<?php echo esc_url(('www.google.cl')); ?>">
+        <img src="<?php bloginfo('template_url')?>/assets/images/instagram-brands.svg" alt="rrss" class="rrss" >
+      </a>
+    <hr>
+    <br>
+    <p> Siguenos </p>
+  </div>
+  </div>
+  </div>
 </footer>
     <!-- footer -->
 
